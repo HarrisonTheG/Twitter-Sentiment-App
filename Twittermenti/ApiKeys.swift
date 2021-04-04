ba52a97043f1fc64bb8ef4847bb9b2acbe891515
Harrison Goenawan <Harrison@Harrisons-MacBook-Pro-3.local>
2021-04-05 01:04:13 +0800

    Adding all files to stage and commit, also adding more comments from previous commit: 2. Created function in VC to fetch api keys from Secrets.plist

--- Twittermenti/ApiKeys.swift
+++ Twittermenti/ApiKeys.swift
@@ -7,3 +7,8 @@
 //
 
 import Foundation
+
+struct ApiKeys: Codable{
+    var apiKey:String
+    var apiSecretKey:String
+}
--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,22 +7,115 @@
 //
 
 import UIKit
+import SwifteriOS
+import CoreML
+import SwiftyJSON
+
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
-
+    
+    
+    let tweetCount = 100
+    let sentimentClassifier = TweetSentimentClassifier()
+    
+    //access API keys from plist (unhandled)
+    let swifter = Swifter(consumerKey:fetchApiKey(value: "api")!, consumerSecret:fetchApiKey(value: "secret")!)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
     }
 
     @IBAction func predictPressed(_ sender: Any) {
-    
+ 
+         fetchTweets()
     
     }
     
+    func fetchTweets(){
+        
+        
+        if let searchText = textField.text {
+          swifter.searchTweet(using: searchText, lang: "en", count: tweetCount, tweetMode: .extended, success: { (results, metadata) in
+              
+              var tweets = [TweetSentimentClassifierInput]()
+              
+             for i in 0..<self.tweetCount {
+              if let tweet = results[i]["full_text"].string {
+                  let tweetInput = TweetSentimentClassifierInput(text: tweet)
+                  tweets.append(tweetInput)}
+                  }
+            let score = self.makePrediction(with: tweets)
+            self.updateUI(with: score)
+          }
+              
+          ) { (error) in
+              print ("there was an error witht the Twitter API request \(error)")
+              }}
+    }
+    func makePrediction(with tweets: [TweetSentimentClassifierInput]) -> Int{
+        var predictionScore = 0
+        do{
+            let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+            for pred in predictions{
+                if pred.label == "Pos"{
+                predictionScore += 1
+                } else if pred.label == "Neg" {
+                    predictionScore -= 1
+                }
+            }
+            
+        }
+        catch {
+            print("Unable to make predictions, \(error)")
+        }
+        return predictionScore
+    }
+    func updateUI(with predictionScore: Int){
+        
+        if predictionScore > 20 {
+            self.sentimentLabel.text = "😍"
+        } else if predictionScore > 10 {
+            self.sentimentLabel.text = "😀"
+        } else if predictionScore > 0 {
+            self.sentimentLabel.text = "🙂"
+        } else if predictionScore == 0 {
+            self.sentimentLabel.text = "😐"
+        } else if predictionScore > -10 {
+            self.sentimentLabel.text = "😕"
+        } else if predictionScore > -20 {
+            self.sentimentLabel.text = "😡"
+        } else {
+            self.sentimentLabel.text = "🤮"
+        }
+    }
 }
+
+//newly added 4/4/2021
+
+func fetchApiKey (value: String) -> String?{
+    
+    
+    guard let filePath = Bundle.main.path(forResource: "Secrets", ofType: "plist") else {
+    fatalError("Couldn't find file 'Secrets.plist'.")
+    }
+    
+    guard let xml = FileManager.default.contents(atPath: filePath) else {
+        fatalError("Couldn't get data with Data data type from path.")
+    }
+    if let apiKeys = try? PropertyListDecoder().decode(ApiKeys.self, from: xml) {
+        if (value == "secret")
+        {return apiKeys.apiSecretKey}
+        else
+        {return apiKeys.apiKey}
+    }
+    return nil
+}
+
+
 
